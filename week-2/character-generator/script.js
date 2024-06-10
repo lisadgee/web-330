73addbe73391cffe14c07bd26836329cd1e28080
Lisa Gee <lisadgee@hotmail.com>
2024-06-09 22:25:30 -0400

    week 2 assignment

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,50 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Lisa Gee
+  Date: June 4, 2024
+  Filename:  script
 */
 
 "use strict";
-
+//Creates character with 3 parameters
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  //Declare private variables and set in body of function
+  //Return an object with functions that expose the character variables  
+  return {
+    getName: function() {
+      return name;
+    },
+
+    getGender: function() {
+      return gender;
+    },
+
+    getClass: function() {
+      return characterClass;
+    }
+  }
 }
 
+//Event listener for button
 document.getElementById("generateHero").addEventListener("click", function(e) {
-  e.preventDefault();
+  e.preventDefault(); //override browser code
 
-  // TODO: Get form values
+  // Get form values
+  let heroName = document.getElementById("heroName").value;
+  let heroGender = document.getElementById("heroGender").value;
+  let heroClass = document.getElementById("heroClass").value;
+  
+  //Create character
+  let character = createCharacter (heroName, heroGender, heroClass);
 
-  // TODO: Create character
+  //Display character information
+  let outputName = document.getElementById("outputName");
+  outputName.innerHTML = character.getName();  //use object to retrieve character values
 
-  // TODO: Display character information
+  let outputGender = document.getElementById("outputGender");
+  outputGender.innerHTML = character.getGender();
+
+  let outputClass = document.getElementById("outputClass");
+  outputClass.innerHTML = character.getClass();
 });
