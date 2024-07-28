7f9beea0926b03620b60a6a526a72b4fe8b8c46f
Lisa Gee <lisadgee@hotmail.com>
2024-07-28 15:23:04 -0400

    submitting week 9

--- week-9/project12-03/project12-03.js
+++ week-9/project12-03/project12-03.js
@@ -10,18 +10,18 @@
       Filename: project12-03.js
 */
 
-
+//Apply click method to article > h2 selector
 $("article > h2").click( e => {
-
+//Declare variables
    let heading = $(e.target);
    let list = $(heading).next();
    let headingImage = $(heading).children("img");
-   
+   //Hide/show content over half-second interval
    $(list).slideToggle(500);
-   
+   //Change symbol in headings and get value of src attribute.  If src equals plus.pg, change to minus.png
    if ($(headingImage).attr("src") === "plus.png") {
       $(headingImage).attr("src", "minus.png");
-   } else {
+   } else {  //else set src to plus.png
       $(headingImage).attr("src", "plus.png");
    }
    
